cf542c7c8eb508b65db88504336e3dd54885e289
karthikeya-r-365 <karthikey.r@webvillee.in>
2023-10-16 12:26:26 +0530

    project SetUP

---
+++ entity/user.ts
@@ -0,0 +1,49 @@
+import { Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+
+/*
+    @Entity() -> This decorator is used to mark classes that will be an entity (table or document depend on database type). Database schema will be created for all classes decorated with it, and Repository can be retrieved and used for it.
+    @Column -> Column decorator is used to mark a specific class property as a table column. Only properties decorated with this decorator will be persisted to the database when entity be saved.
+*/
+
+@Entity()
+export class User {
+
+   @PrimaryGeneratedColumn()
+    id! :Number;
+
+    // Initialize Properties in the Constructor
+    // You can initialize your properties in the constructor. Here's how you can do it:
+    @Column({default: "guset"})
+    name:String  = "";
+
+    //Use the ! Non-null Assertion Operator
+    //You can use the non-null assertion operator ! to tell TypeScript that you're certain that the properties will be initialized before they are used:
+    @Column({
+        unique: true,
+    })
+    user_phone_number! :string;
+
+    @Column({
+        unique: true,
+    })
+    email! : String;
+
+    @Column({
+        default: false
+    })
+    verify_number! : Boolean;
+
+    @Column({
+        default: false
+    })
+    verify_email! : Boolean;
+
+    @Column()
+    passowrd! : String;
+
+    @Column()
+    created_at! : Date
+
+    @Column()
+    updated_at! : Date
+};
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const error_message = {
+    IN_VALID_NUMBER:"not a valid Phone Number",
+    IN_VALID_EMAIL: "please enter a valid email address",
+};
+
